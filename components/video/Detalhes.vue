b997b9acfc840f8efb6624ab46a9aef0193e8917
Keidson Roby <keidsondesigner@gmail.com>
2024-04-05 22:15:31 -0400

    feat: :sparkles: Primeiro Composable

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,4 +1,13 @@
 <template>
-  <h1>Componente - Nuxt 3</h1>
-  <p>Autor: Keidson</p>
+  <div>
+    <h1>Componente - Nuxt 3</h1>
+    <p>Autor: Keidson</p>
+    <p>{{ diferencaEmDias }} dias atrás</p>
+  </div>
 </template>
+<script setup lang="ts">
+const { diasPassados, diferencaEmDias } = useFormatacao();
+const dataVideo = new Date(2023, 9, 2);
+
+diasPassados(dataVideo);
+</script>
