76c06e82a7380cd8c293b368214d0fa6d6f99cee
Keidson Roby <keidsondesigner@gmail.com>
2024-04-11 18:04:33 -0400

    feat(backend): :sparkles: Controller buscarVideoPorId; implementação do endpoint "/videos/:id"

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -3,5 +3,6 @@ import * as videoController from "../controller/video";
 const router = createRouter();
 
 router.get('/videos', defineEventHandler(videoController.buscaVideos));
+router.get('/videos/:id', defineEventHandler(videoController.buscaVideoPorId))
 
 export default useBase("/api/v1", router.handler);
--- server/controller/video.ts
+++ server/controller/video.ts
@@ -1,5 +1,7 @@
 import { Video } from "@/interfaces/video.interface"
 import { postgresClient } from "../utils"
+import { H3Event } from "h3";
+
 
 const { client } = postgresClient();
 
@@ -7,4 +9,13 @@ export const buscaVideos = async () => {
   const resultado = await client.query('SELECT * FROM videos')
   
   return resultado.rows as Video[];  
+}
+
+export const buscaVideoPorId = async (event: H3Event) => {
+  const resquestId = (await event.context.params?.id) as string
+  const resultado = await client.query(
+    "SELECT * FROM videos WHERE id = $1", [resquestId]
+  );
+  
+  return resultado.rows[0] as Video;  
 }
