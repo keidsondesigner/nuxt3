3e0f70f3522941a0619904770a358574cdf619af
Keidson Roby <keidsondesigner@gmail.com>
2024-04-12 15:14:22 -0400

    feat(frontend): :sparkles: Tela Detalhes video id: Implementação da função editarVideo()

--- pages/videos/[id].vue
+++ pages/videos/[id].vue
@@ -1,7 +1,7 @@
 <template>
 <Dialog>
   <!-- <div>
-    video por id: {{ idParams }}
+    video por id: {{ route.params.id }}
   </div> -->
   <div class="conatiner-principal m-8">
     <div class="flex justify-between mb-8 items-end">
@@ -17,9 +17,9 @@
           <Plus />
         </Button>
         <Button
-        class="gap-2 mr-4"
-        variant="destructive"
-        @click="editarFavorito(video)"
+          class="gap-2 mr-4"
+          variant="destructive"
+          @click="excluirVideo"
         >
           Excluír
           <SquarePen />
@@ -27,8 +27,8 @@
         <DialogTrigger as-child>
           <Button
             class="gap-2 mr-4"
+            @click="abrirModal"
             variant="secondary"
-            @click="editarFavorito(video)"
           >
             Editar
             <SquarePen />
@@ -39,17 +39,16 @@
   </div>
   <section
     class="max-w-4xl mx-auto flex flex-col items-center p-5 rounded-xl border bg-card text-card-foreground shadow"
-
-    >
-      <iframe 
-        class="w-full aspect-video rounded"
-        :src="video?.url"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      />
+  >
+    <iframe 
+      class="w-full aspect-video rounded"
+      :src="video?.url"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    />
   </section>
   <DialogContent class="max-w-[360px] sm:max-w-[425px] md:max-w-[640px]">
     <DialogHeader>
@@ -58,25 +57,41 @@
         Para salvar alterações, basta clicar no botão confirmar.
       </DialogDescription>
     </DialogHeader>
-    <div class="grid gap-4 py-4">
-      <div class="grid grid-cols-4 items-center gap-4">
-        <Label for="name" class="text-right">
-          Descrição
-        </Label>
-        <Input type="text" id="name" value="Pedro Duarte" class="col-span-3" />
+    <form 
+      @submit.prevent="editarVideo"
+    >
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="descricao" class="text-right">
+            Descrição
+          </Label>
+          <Input
+            class="col-span-3"
+            v-model="stateForm.descricao"
+            type="text"
+            id="descricao"
+            required
+          />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="url" class="text-right">
+            Url
+          </Label>
+          <Input 
+            class="col-span-3" 
+            v-model="stateForm.url"
+            type="text" 
+            id="url" 
+            required
+          />
+        </div>
       </div>
-      <div class="grid grid-cols-4 items-center gap-4">
-        <Label for="username" class="text-right">
-          Url
-        </Label>
-        <Input type="text" id="username" value="@peduarte" class="col-span-3" />
-      </div>
-    </div>
-    <DialogFooter>
-      <Button type="submit">
-        Confirmar
-      </Button>
-    </DialogFooter>
+      <DialogFooter>
+        <Button type="submit">
+          Confirmar
+        </Button>
+      </DialogFooter>
+    </form>
   </DialogContent>
 </Dialog>
 </template>
@@ -96,7 +111,7 @@ import {
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-import type { Video } from '~/interfaces/video.interface';
+import type { Video } from '@/interfaces/video.interface';
 
 // define o estilo de layout, que vai ser mostrado nesta página;
 definePageMeta({
@@ -105,18 +120,25 @@ definePageMeta({
   middleware: ["video", "auth"],
 })
 const route = useRoute();
-const idParams = computed(() => route.params.id);
-
+const router = useRouter();
 const { $toast } = useNuxtApp();
 
-// Buscar o video por id
+// Store e Storage
+const { adicionarFavorito } = useVideoStore();
+
+// API - Buscar o video por id
 const { data: video, error } = await useFetch(`/api/v1/videos/${route.params.id}`);
 if(error.value) {
   console.log(error.value)
   $toast.error("Erro ao carregar o vídeo");
 }
 
-const { adicionarFavorito } = useVideoStore();
+// Estados reativos
+const stateForm = reactive({
+  id: 0,
+  descricao: "",
+  url: "",
+});
 
 // Adicionar o video aos favoritos no store + LocalStorage
 function addFavorito(video: Video) {
@@ -124,13 +146,31 @@ function addFavorito(video: Video) {
   $toast.success(`${video.descricao} - foi adicionado aos favoritos`);
 }
 
-function editarFavorito(video: Video) {
-  console.log(video)
+function excluirVideo() {
+  console.log("excluir")
 }
 
-onMounted(async () => {
-  video.value = await $fetch(`/api/v1/videos/${route.params.id}`)
-})
+function abrirModal() {
+  stateForm.descricao = video.value.descricao;
+  stateForm.url = video.value.url;
+  stateForm.id = video.value.id;
+};
+
+async function editarVideo() {
+  try {
+    await $fetch(`/api/v1/videos/${route.params.id}`, {
+      method: "PUT",
+      body: stateForm,
+    });
+
+    $toast.success("Vídeo atualizado com sucesso!");
+    setTimeout(() => {
+      router.push("/videos");
+    }, 2000)
+
+  } catch (error) {
+    $toast.error("Erro ao atualizar o vídeo");
+    console.log(error)
+  }
+}
 </script>
-<style scoped>
-</style>
