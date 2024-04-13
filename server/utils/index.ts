51bcd7f0f070572c91d384bb82671cc814a909d5
Keidson Roby <keidsondesigner@gmail.com>
2024-04-12 20:11:30 -0400

    feat(frontend): :sparkles: ENV de produção

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,13 +1,24 @@
 import pg from "pg";
 
+// Ambiente de desenvolvimento
+// export const postgresClient = () => {
+//   const client = new pg.Pool({
+//     host: process.env.POSTGRES_HOST,
+//     database: process.env.POSTGRES_DATABASE,
+//     port: Number(process.env.POSTGRES_PORT),
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//   });
 
+//   return {
+//     client,
+//   };
+// };
+
+// Ambiente de produção
 export const postgresClient = () => {
   const client = new pg.Pool({
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
+    connectionString: process.env.POSTGRES_URL ,
   });
 
   return {
